8d48f6def820105cf52242f3db4308f5ff08d593
Fantazindy-KR <leehyen4965@gmail.com>
2022-04-16 00:18:19 +0900

    Improve UI and adapt gsap to healthbar

--- index.js
+++ index.js
@@ -178,6 +178,8 @@ function animate() {
   c.fillRect(0, 0, canvas.width, canvas.height);
   background.update();
   shop.update();
+  c.fillStyle = "rgba(255, 255, 255, 0.1)";
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
 
@@ -228,8 +230,9 @@ function animate() {
   ) {
     enemy.takeHit();
     player.isAttacking = false;
-    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
+    // document.querySelector("#enemyHealth").style.width = enemy.health + "%";
     console.log("player attack");
+    gsap.to("#enemyHealth", { width: enemy.health + "%" });
   }
 
   //if player misses
@@ -245,9 +248,9 @@ function animate() {
   ) {
     player.takeHit();
     enemy.isAttacking = false;
-    player.health -= 20;
-    document.querySelector("#playerHealth").style.width = player.health + "%";
+    // document.querySelector("#playerHealth").style.width = player.health + "%";
     console.log("enemy attack");
+    gsap.to("#playerHealth", { width: player.health + "%" });
   }
 
   //if enemy misses
